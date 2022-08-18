b00f341ad7024c5601e2e5d10c2ba206aef47f1b
Moni <monika.berke@gmail.com>
2022-08-18 23:06:56 +0200

    seconds working

--- solutions/getSecondsOfThisYearUntilNow.js
+++ solutions/getSecondsOfThisYearUntilNow.js
@@ -1,3 +1,12 @@
+'use strict';
+
+const now = new Date();
+const presentDate = new Date();
+const startDate = new Date(presentDate.getFullYear(), 0, 1);
+const getSecondsOfThisYearUntilNow = () => {
+    result = ((now.getTime() - startDate.getTime()) / 1000);
+    return (result);
+};
 
 export default getSecondsOfThisYearUntilNow;
 
